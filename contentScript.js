7a7981a2ebbaf736e0880c2a8676db732b5f8667
hydrol0x <mrjacobry@gmail.com>
2022-07-06 18:26:06 -0400

    Started Fetch URLS

--- contentScript.js
+++ contentScript.js
@@ -3,8 +3,8 @@
     let listings = "";
     let buttonId = 0;
     let listingUrl = ""
-    let storedUrls = []
-
+    let removedUrls = [];
+ 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, value, searchId } = obj;
     
@@ -14,21 +14,35 @@
         }
     });
 
-    const fetchBookmarks = () => {
-        return new Promise((resolve) => {
-            chrome.storage.sync.get([currentSearch], (obj) => {
-                resolve(obj[currentSearch] ? JSON.parse(obj[currentSearch]): [])
-            })
-        })
+    const fetchUrls = async () => {
+        let urls = [];
+        await chrome.storage.sync.get(null, (result) => {
+            let allVals = Object.values(result);
+            for(const value of allVals){
+                urls.push(value[0].url);
+            };
+        });
+        return urls ? urls : [];
     }
 
-    const newSearchLoaded = () => {
-        const deleteBtnExists = document.getElementsByClassName("delete-btn")[0];
+    const newSearchLoaded = async () => {
 
+        const deleteBtnExists = document.getElementsByClassName("delete-btn")[0];
+        // list of search results
         listings = document.querySelector(".srp-results").querySelectorAll(".s-item");
-        
+        // list of removed URLs
+        removedUrls = await fetchUrls();
         // loop over each listing and add a button
+        console.log(removedUrls)
         listings.forEach((listing) => {
+            // First check for removed listings
+            listingUrl = listing.getElementsByClassName("s-item__link")[0].href;
+            removedUrls.forEach((url) => {
+                console.log(url);
+                if (listingUrl === url){
+                    listing.style.backgroundColor = "red";
+                }
+            });
             // if the button doesn't already exist add it
             if (!deleteBtnExists) {
                 const deleteBtn = document.createElement("button");
@@ -39,7 +53,6 @@
 
                 listing.appendChild(deleteBtn);
                 // on click remove listing and record url
-                listingUrl = listing.getElementsByClassName("s-item__link")[0].href;
                 deleteBtn.addEventListener("click", (e) => {
                     // can add more information later
                     const newUrl = {
