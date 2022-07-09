afba02643fa7dd3754196741ad707c42b868b091
hydrol0x <mrjacobry@gmail.com>
2022-07-09 17:47:27 -0400

    Fixed <<receiving end does not exist>> error

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("ebay.com/sch")) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.url && tab.url.includes("ebay.com/sch")) {
     const queryParameters = tab.url.split(/3&_/)[1];
+    console.log(queryParameters)
     const urlParameters = new URLSearchParams(queryParameters);
-    console.log(urlParameters)
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
