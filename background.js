6bff8937939bccd5aa6e79170c3faeebe4efaa16
hydrol0x <mrjacobry@gmail.com>
2022-07-10 09:45:45 -0400

    Changed searchId from URL params to queryParams

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.url && tab.url.includes("ebay.com/sch")) {
-    const queryParameters = tab.url.split(/3&_/)[1];
+    const queryParameters = tab.url.split(/from/)[1];
     console.log(queryParameters)
     const urlParameters = new URLSearchParams(queryParameters);
-
+    console.log(urlParameters.get("nkw"))
     chrome.tabs.sendMessage(tabId, {
+      search: queryParameters,
       type: "NEW",
-      searchID: urlParameters.get("nkw")
     });
   }
 })
