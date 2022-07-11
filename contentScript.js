4cc3f72114f251cde9afc47300dac909b322e465
hydrol0x <mrjacobry@gmail.com>
2022-07-11 09:21:23 -0400

    return promise and use async/await to access currentSearch from callback func

--- contentScript.js
+++ contentScript.js
@@ -1,92 +1,102 @@
 (() => {
-    let currentSearch = "";
-    let listingId = 0;
-    let listingUrl = "";
-    let listingUrls = {};
-    let removedUrls = [];
- 
-    // TODO: learn how to use callback func and access currentSearch
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, search } = obj;
-        if (type === "NEW") {
-            currentSearch = search; 
-            newSearchLoaded();
-        }
+  let currentSearch = "";
+  let listingId = 0;
+  let listingUrl = "";
+  let listingUrls = {};
+  let removedUrls = [];
+
+  // TODO: learn how to use callback func and access currentSearch
+  let pSearchId = new Promise((resolve) => {
+    chrome.runtime.onMessage.addListener((obj) => {
+      const { type, value, search } = obj;
+      if (type === "NEW") {
+        resolve((currentSearch = search));
+        newSearchLoaded();
+      }
     });
+  });
 
-    const fetchUrls = () => {
-        return new Promise((resolve) => {
-            chrome.storage.sync.get(null, (obj) => {
-                resolve(Object.values(obj).length>0 ? Object.values(obj) : [])
-            });
+  const fetchUrls = () => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get(null, (obj) => {
+        resolve(Object.values(obj).length > 0 ? Object.values(obj) : []);
+      });
+    });
+  };
+
+  const newSearchLoaded = async () => {
+    const searchResults = document
+      .querySelectorAll(".srp-results")[0]
+      .querySelectorAll(".s-item"); // list of search results
+    removedUrls = await fetchUrls(); // removed urls
+    currentSearch = await pSearchId;
+
+    searchResults.forEach((listing) => {
+      // TODO: button ID is search ID
+      const id = `listing${listingId}${currentSearch}`;
+      const deleteBtnExists = document.getElementById(id);
+      const deleteBtn = document.createElement("button");
+      if (!deleteBtnExists) {
+        deleteBtn.className = "delete-btn " + "btn";
+        deleteBtn.textContent = "x";
+        deleteBtn.title = "Remove listing";
+        deleteBtn.id = id;
+
+        // button styling
+        listing.style.paddingBottom = "2rem";
+        deleteBtn.style.height = "25px";
+        deleteBtn.style.width = "25px";
+        deleteBtn.style.fontSize = "10pt";
+        deleteBtn.style.float = "right";
+        deleteBtn.style.marginRight = "0.2%";
+        deleteBtn.style.backgroundColor = "white";
+        deleteBtn.style.borderWidth = "1.5px";
+        deleteBtn.style.borderRadius = "50%";
+        deleteBtn.onmouseover = () => {
+          deleteBtn.style.color = "red";
+          deleteBtn.style.borderColor = "red";
+        };
+        deleteBtn.onmouseout = () => {
+          deleteBtn.style.color = "black";
+          deleteBtn.style.borderColor = "black";
+        };
+        deleteBtn.style.transition = "all 0.25s";
+
+        listing.appendChild(deleteBtn);
+      }
+
+      listingUrl = listing.getElementsByClassName("s-item__link")[0].href; // get listing URL
+      listingUrls[deleteBtn.id] = listingUrl; // add unique id to each url and add to the list of all urls
+
+      for (const url of removedUrls) {
+        // Check for removed listings
+        if (listingUrl.split("&")[0] === url[0].url.split("&")[0]) {
+          // urls change but are the same before &
+          console.log(`removed listing with url ${url}`);
+          listing.remove();
+        }
+      }
+
+      // on click remove listing and record url
+      deleteBtn.addEventListener("click", (e) => {
+        // can add more information later
+        let newUrl = {
+          url: listingUrls[deleteBtn.id],
+          name: "name",
+        };
+
+        // store in chrome storage
+        // key = button id; value is url.
+        chrome.storage.sync.set({
+          [deleteBtn.id]: [newUrl],
         });
-    }
 
-    const newSearchLoaded = async () => {
+        // remove the listing on button click
+        listing.remove();
+      });
 
-
-        const searchResults = document.querySelectorAll(".srp-results")[0].querySelectorAll(".s-item");     // list of search results
-        removedUrls = await fetchUrls();    // removed urls
-
-        searchResults.forEach((listing) => {   
-            // TODO: button ID is search ID
-            const deleteBtnExists = document.getElementById(`listing${listingId}`);
-            const deleteBtn = document.createElement("button");
-            if (!deleteBtnExists){
-                deleteBtn.className = "delete-btn";
-                deleteBtn.textContent = "x";
-                deleteBtn.title = "Remove listing";
-                deleteBtn.id = `listing${listingId}`; // ${searchId}`
-
-                // button styling
-                listing.style.paddingBottom = '2rem';
-                deleteBtn.style.height = '25px';
-                deleteBtn.style.width = '25px';
-                deleteBtn.style.fontSize = '10pt';
-                deleteBtn.style.float = 'right';
-                deleteBtn.style.marginRight = '0.2%';
-                deleteBtn.style.backgroundColor = 'white';
-                deleteBtn.style.borderWidth = '1.5px'
-                deleteBtn.style.borderRadius = "50%";
-                deleteBtn.onmouseover = () => {deleteBtn.style.color = 'red'; deleteBtn.style.borderColor='red';}
-                deleteBtn.onmouseout = () => {deleteBtn.style.color = 'black'; deleteBtn.style.borderColor='black';}
-                deleteBtn.style.transition = 'all 0.25s';
-
-                listing.appendChild(deleteBtn);
-            }
-
-            listingUrl = listing.getElementsByClassName("s-item__link")[0].href;    // get listing URL
-            listingUrls[deleteBtn.id] = listingUrl  // add unique id to each url and add to the list of all urls
-
-            for(const url of removedUrls){  // Check for removed listings 
-                if (listingUrl.split("&")[0] === url[0].url.split("&")[0]){ // urls change but are the same before & 
-                    console.log(`removed listing with url ${url}`);
-                    listing.remove();
-                }
-            };
-
-            // on click remove listing and record url
-            deleteBtn.addEventListener("click", (e) => {
-                // can add more information later
-                let newUrl = {
-                    url: listingUrls[deleteBtn.id], 
-                    name: "name",
-                };
-                
-                // store in chrome storage
-                // key = button id; value is url.
-                chrome.storage.sync.set({
-                    [deleteBtn.id]: [newUrl] 
-                });
-
-                // remove the listing on button click
-                listing.remove();
-            });
-
-                // next button will have id+1
-                listingId++
-        
-        });
-    }
+      // next button will have id+1
+      listingId++;
+    });
+  };
 })();
-
