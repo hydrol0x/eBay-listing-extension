31eb706bf06c5f3c0de617a96fd00b113d90d6df
hydrol0x <mrjacobry@gmail.com>
2022-07-11 10:17:38 -0400

    Removed URLparams from background.js

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("ebay.com/sch")) {
+  if (
+    changeInfo.status === "complete" &&
+    tab.url &&
+    tab.url.includes("ebay.com/sch")
+  ) {
     const queryParameters = tab.url.split(/from/)[1];
-    console.log(queryParameters)
-    const urlParameters = new URLSearchParams(queryParameters);
-    console.log(urlParameters.get("nkw"))
     chrome.tabs.sendMessage(tabId, {
       search: queryParameters,
       type: "NEW",
     });
   }
-})
+});
